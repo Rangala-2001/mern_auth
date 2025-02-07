d37e25874c2c773846e7fce023630edaf426a4a3
Joshna <118729274+Rangala-2001@users.noreply.github.com>
2025-02-07 18:31:04 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
-require('dotenv').config();  // Load environment variables from .env
+// config/keys.js
+require('dotenv').config();
 
 module.exports = {
-  mongoURI: process.env.MONGO_URI,  // Get Mongo URI from .env
-  secretOrKey: process.env.JWT_SECRET  // Get JWT secret from .env
+  mongoURI: process.env.MONGO_URI || "mongodb://mongo:27017/mern_auth",
+  secretOrKey: process.env.JWT_SECRET || "56e801ce94f48b0c0c754a89f38d990fc34428c186d1b87093884f633cfca5d6"
 };
-
